c10bcbd780ac851ebc11462eefd85f65e5a9582f
Anburaj S <anburaj2203@gmail.com>
2023-10-30 20:06:20 +0530

    added filters

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Box, Spinner } from "@chakra-ui/react";
+import { Box, Select, Spinner } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import DoctorCard from "../components/DoctorCard";
@@ -7,36 +7,54 @@ import { useParams } from "react-router-dom";
 const Dashboard = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
-  const [status,setStatus]=useState(false)
+  const [status, setStatus] = useState(false);
+  const [specialization, setSpecialization] = useState("");
+
   useEffect(() => {
     // getData()
     setLoading(true);
-    fetch(`https://hospital-601o.onrender.com/doctors`, {
-    // fetch(`http://localhost:8080/doctors`, {
+    if (specialization) {
+      // fetch(`https://hospital-601o.onrender.com/doctors?specialization=${specialization}`, {
+      fetch(`http://localhost:8080/doctors?specialization=${specialization}`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          setLoading(false);
+          console.log(res);
+          setData(res);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      // fetch(`https://hospital-601o.onrender.com/doctors`, {
+      fetch(`http://localhost:8080/doctors`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          setLoading(false);
+          console.log(res);
+          setData(res);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [status, specialization]);
+
+  const handleDelete = (id) => {
+    fetch(`https://hospital-601o.onrender.com/doctors/delete/${id}`, {
+      // fetch(`http://localhost:8080/doctors/delete/${id}`, {
+      method: "DELETE",
       headers: {
         Authorization: localStorage.getItem("token"),
       },
     })
       .then((res) => res.json())
       .then((res) => {
-        setLoading(false);
-        console.log(res);
-        setData(res);
-      })
-      .catch((err) => console.log(err));
-  }, [status]);
-
-  const handleDelete = (id) => {
-    fetch(`https://hospital-601o.onrender.com/doctors/delete/${id}`, {
-    // fetch(`http://localhost:8080/doctors/delete/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: localStorage.getItem("token"),
-      }
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setStatus(true)
+        setStatus(true);
       })
       .catch((err) => console.log(err));
   };
@@ -54,6 +72,19 @@ const Dashboard = () => {
           />
         </Box>
       )}
+
+        <SELECTDIV>
+          <Select
+            placeholder="Select Specialization"
+            value={specialization}
+            onChange={(e) => setSpecialization(e.target.value)}
+          >
+            <option value="Cardiologist">Cardiologist</option>
+            <option value="Dermatologist">Dermatologist</option>
+            <option value="Pediatrician">Pediatrician</option>
+            <option value="Psychiatrist ">Psychiatrist </option>
+          </Select>
+        </SELECTDIV>
       <DIV>
         {data.map((el) => {
           return (
@@ -75,3 +106,15 @@ const DIV = styled.div`
   grid-template-columns: repeat(3, 1fr);
   gap: 20px;
 `;
+
+const SELECTDIV=styled.div`
+ width: 60%;
+ margin: auto;
+ margin-top: 15px;
+ font-size: 18px;
+ background-color: white;
+ select{
+     border-radius: 15px;
+ }
+ border-radius: 15px;
+`
